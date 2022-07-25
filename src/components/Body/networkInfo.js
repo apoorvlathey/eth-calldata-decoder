0a8cad14022b011290e77511d78226239200b998
CodinMaster <latheyapoorv@gmail.com>
2022-07-26 03:03:35 +0530

    Add missing EVM chains

--- src/components/Body/networkInfo.js
+++ src/components/Body/networkInfo.js
@@ -5,15 +5,36 @@ const networkInfo = [
     api: `https://api.etherscan.io/api?module=contract&action=getabi&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`,
   },
   {
-    chainID: 137,
-    name: "Polygon",
-    api: `https://api.polygonscan.com/api?module=contract&action=getabi&apikey=${process.env.REACT_APP_POLYGONSCAN_API_KEY}`,
+    chainID: 42161,
+    name: "Arbitrum One",
+    api: `https://api.arbiscan.io/api?module=contract&action=getabi&apikey=${process.env.REACT_APP_ARBISCAN_API_KEY}`,
+  },
+  {
+    chainID: 43114,
+    name: "Avalanche",
+    api: `https://api.snowtrace.io/api?module=contract&action=getabi&apikey=${process.env.REACT_APP_SNOWTRACE_API_KEY}`,
   },
   {
     chainID: 56,
     name: "Binance Smart Chain",
     api: `https://api.bscscan.com/api?module=contract&action=getabi&apikey=${process.env.REACT_APP_BSCSCAN_API_KEY}`,
   },
+  {
+    chainID: 250,
+    name: "Fantom Opera",
+    api: `https://api.ftmscan.com/api?module=contract&action=getabi&apikey=${process.env.REACT_APP_FTMSCAN_API_KEY}`,
+  },
+  {
+    chainID: 10,
+    name: "Optimism",
+    api: `https://api-optimistic.etherscan.io/api?module=contract&action=getabi&apikey=${process.env.REACT_APP_OPTIMISM_API_KEY}`,
+  },
+  {
+    chainID: 137,
+    name: "Polygon",
+    api: `https://api.polygonscan.com/api?module=contract&action=getabi&apikey=${process.env.REACT_APP_POLYGONSCAN_API_KEY}`,
+  },
+
   {
     chainID: 42,
     name: "Kovan Testnet",
